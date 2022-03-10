d6cb5fe3953eaa56a8ab1246b006a3691c84499f
taichi <taichi.01gr14@gmail.com>
2022-03-10 16:06:23 +0900

    products.regist

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,5 +1,7 @@
 <!--　ヘッダー　-->
-<script src="https://code.jquery.com/jquery-3.0.0.js" integrity="sha256-jrPLZ+8vDxt2FnE1zvZXCkCcebI/C8Dt5xyaQBjxQIo=" crossorigin="anonymous"></script>
+{{--<script src="https://code.jquery.com/jquery-3.0.0.js" integrity="sha256-jrPLZ+8vDxt2FnE1zvZXCkCcebI/C8Dt5xyaQBjxQIo=" crossorigin="anonymous"></script>--}}
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
 <header class="header">
     <div class="header-left">
         <?php if(!empty(session('login_date'))): ?>
