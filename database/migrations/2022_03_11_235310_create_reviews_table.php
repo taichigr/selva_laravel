8152f17f3eb4177dc3bffae72999fd13073306dc
taichi <taichi.01gr14@gmail.com>
2022-03-11 23:58:36 +0900

    make migration reviews

---
+++ database/migrations/2022_03_11_235310_create_reviews_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->bigIncrements('id')->comment('コメントID');
+            $table->integer('member_id')->comment('会員ID');
+            $table->integer('product_id')->comment('商品ID');
+            $table->integer('evaluation')->comment('評価');
+            $table->text('comment')->comment('商品コメント');
+            $table->timestamps();
+            $table->softDeletes()->comment('削除日時');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
